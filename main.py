7ee38289ed7e8954c2365516616ea70b44490035
Samuel Schapiro <87241547+samjschapiro@users.noreply.github.com>
2024-04-01 12:25:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
 from sam import SAM
-from ssam import SSAM
-import projgrad
 import os
 import argparse
 from copy import deepcopy
@@ -25,7 +23,6 @@ parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 parser.add_argument('--optimizer', default='sgd')
-parser.add_argument('--ssam_lambda', default=0.5)
 parser.add_argument('--model', default='resnet18')
 parser.add_argument('--pretrained', default=True)
 parser.add_argument('--epochs', default=200, type=int)
@@ -129,13 +126,10 @@ if args.optimizer == 'sgd':
 elif args.optimizer == 'sam':
     optimizer = SAM(net.parameters(), optim.SGD, rho=0.05, lr=args.lr, momentum=0.9, weight_decay=5e-4)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer.base_optimizer, T_max=200, last_epoch=start_epoch-1)    
-else:
-    optimizer = SSAM(net.parameters(), optim.SGD, rho=0.05, lr=args.lr, momentum=0.9, weight_decay=5e-4, lam=args.ssam_lambda)
-    if args.resume:
-        optimizer.load_state_dict(opt_state_dict)
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer.base_optimizer, T_max=200, last_epoch=start_epoch-1)    
-    if args.resume:
-        scheduler.load_state_dict(sch_state_dict)
+
+##########################
+# TODO: Add rho schedule #
+##########################
 
 # Training
 def train(epoch):
@@ -153,48 +147,23 @@ def train(epoch):
     train_loss = 0
     correct = 0
     total = 0
-    if args.optimizer == 'ssam':
-        avg_incr_sharp = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         enable_bn(net)
         optimizer.zero_grad()
         inputs, targets = inputs.to(device), targets.to(device)
-        if args.optimizer == 'sam' or args.optimizer == 'ssam':
+        if args.optimizer == 'sam':
             inputs_, targets_ = deepcopy(inputs), deepcopy(targets)
             inputs_1, targets_1 = deepcopy(inputs), deepcopy(targets)
             inputs_2, targets_2 = deepcopy(inputs), deepcopy(targets)
-        if args.optimizer == 'ssam':
-            inputs_prep = deepcopy(inputs)
 
         outputs = net(inputs)
-        # Copy of network to apply criterion to SAM update
-        if args.optimizer == 'ssam':
-            copy_of_net = deepcopy(net)
-            copy_of_optimizer = SAM(copy_of_net.parameters(), optim.SGD, rho=0.05, lr=args.lr, momentum=0.9, weight_decay=5e-4)
-            outputs_1 = copy_of_net(inputs_1)
         if args.optimizer == 'sam' or args.optimizer == 'ssam':
-            if args.optimizer == 'ssam':
-                loss_f = torch.mean(net(inputs_prep.cuda()))    
-                loss_f.backward()
-                optimizer.prep(zero_grad=True)
             loss = criterion(outputs, targets)
-            if args.optimizer == 'ssam':
-            # Apply SAM update to copies 
-                loss_1 = criterion(outputs_1, targets_1)
-                loss_1.backward()
-                copy_of_optimizer.first_step(zero_grad=True)
-                sam_loss = criterion(copy_of_net(inputs_2), targets_2) 
             loss.backward()
-            if args.optimizer == 'ssam':
-                optimizer.first_step(zero_grad=True, n_iter=5)
-            else:
-                optimizer.first_step(zero_grad=True)
+              optimizer.first_step(zero_grad=True)
             disable_bn(net)
-            ssam_loss = criterion(net(inputs_), targets_)
             criterion(net(inputs_), targets_).backward()
             optimizer.second_step(zero_grad=True)
-            if args.optimizer == 'ssam':
-                avg_incr_sharp += ssam_loss.item() - sam_loss.item()
         elif args.optimizer == 'sgd':
             loss = criterion(outputs, targets)
             loss.backward()
@@ -205,16 +174,11 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        if args.optimizer != 'ssam':
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
-        else:
-            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d) | Avg Incr. Sharp %.3f'
-                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total, avg_incr_sharp/(batch_idx+1)))
-    if args.optimizer == 'ssam':
-        return train_loss, 100.*correct/total, avg_incr_sharp
-    else:
-        return train_loss, 100.*correct/total
+  
+          progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+      return train_loss, 100.*correct/total
         
 
 def test(epoch):
@@ -255,20 +219,11 @@ def test(epoch):
     return acc
 
 for epoch in range(start_epoch, start_epoch+args.epochs):
-    if args.optimizer == 'ssam':
-        train_loss, train_acc, avg_incr_sharp = train(epoch)
-    else:
-        train_loss, train_acc = train(epoch)
+    train_loss, train_acc = train(epoch)
     test_acc = test(epoch)
     scheduler.step()
     print("Epoch: ", epoch, "Best Acc: ", best_acc)
     with open(f"logs/model:{args.model}_pretrained:{args.pretrained}_dataset:{args.dataset}_opt:{args.optimizer}.txt", "a") as f:
         if epoch == 0:
-            if args.optimizer == 'ssam':
-                f.write("epoch, train_loss, train_acc, test_acc, avg_incr_sharp\n")
-            else:
-                f.write("epoch, train_loss, train_acc, test_acc\n")
-        if args.optimizer == 'ssam':
-            f.write(f"{epoch},{train_loss},{train_acc},{test_acc},{avg_incr_sharp}\n")
-        else:
-            f.write(f"{epoch},{train_loss},{train_acc},{test_acc}\n")
+            f.write("epoch, train_loss, train_acc, test_acc\n")
+        f.write(f"{epoch},{train_loss},{train_acc},{test_acc}\n")
